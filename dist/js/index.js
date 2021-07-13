83ea04642b73ad74f1f6d9e033b861cc660e2506
Cyril Tiger <cyriltiger@gmail.com>
2021-07-13 11:40:32 +0200

    WIP - refactoring - index.js added initMaps

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,72 +1,49 @@
-import AuxiliarySearch from './auxiliarySearch.js';
-import MainSearch from './mainSearch.js';
-import Recipe from './recipe.js';
 import recipes from './recipes.js';
 
-// TEMP method to include recipe list in DOM
-// const getRecipes = () => {
-//   // const displayedRecipes = [];
-
-//   const container = document.createElement('div');
-//   container.id = 'jsResults';
-//   container.className = 'result-group';
-
-//   recipes.forEach((item) => {
-//     const recipe = new Recipe(item);
-//     container.appendChild(recipe.getDOM());
-//   });
-
-//   return container;
-// };
-
-const getDOM = () => {
-  const form = document.getElementById('jsForm');
-  const ingredientAuxiliary = new AuxiliarySearch('ingredients', 'Ingrédients');
-  const applianceAuxiliary = new AuxiliarySearch('appliance', 'Appareil');
-  const ustensilAuiliary = new AuxiliarySearch('ustensils', 'Ustensiles');
-
-  const filterTags = document.createElement('div');
-  filterTags.id = 'jsTags';
-  filterTags.className = 'filter-tags';
-
-  const auxiliarySearchGroup = document.createElement('div');
-  auxiliarySearchGroup.className = 'auxiliary-search-group';
-  auxiliarySearchGroup.append(
-    ingredientAuxiliary.getDOM(),
-    applianceAuxiliary.getDOM(),
-    ustensilAuiliary.getDOM(),
-  );
-
-  form.append(MainSearch.getDOM(), filterTags, auxiliarySearchGroup);
-
-  // TEMP display of results
-  // form.appendChild(getRecipes());
-
-  // DOM Events
-
-  // closing auxiliary search
-  const searchLabels = Array.from(document.querySelectorAll('.auxiliary-search label'));
-  window.addEventListener('click', (e) => {
-    const isTargetLabel = searchLabels.find((label) => label === e.target);
-    const openSearch = document.querySelector('.open');
-    if (isTargetLabel) {
-      const parent = e.target.closest('.auxiliary-search');
-      if (openSearch && !parent.classList.contains('open')) {
-        openSearch.classList.remove('open');
-        parent.classList.add('open');
-      } else {
-        parent.classList.add('open');
-      }
-    } else if (!e.target.closest('.auxiliary-search')) {
-      if (openSearch) {
-        openSearch.classList.remove('open');
-      }
-    }
-  });
+const setMap = (map, key, value) => {
+  if (map.get(key) === undefined) {
+    map.set(key, [value]);
+  } else {
+    const arr = map.get(key);
+    arr.push(value);
+  }
 };
 
+// Set 5 different maps, each with a specific type of key and an array of recipe objects as value
+// keys are : title, description, ingredient, appliance, ustensil
+const initMaps = () => {
+  const titleMap = new Map();
+  const descriptionMap = new Map();
+  const ingredientMap = new Map();
+  const applianceMap = new Map();
+  const ustensilMap = new Map();
+
+  recipes.forEach((recipe) => {
+    const { name, ingredients, description, appliance, ustensils } = recipe;
+
+    setMap(titleMap, name, recipe);
+    setMap(descriptionMap, description, recipe);
+    setMap(applianceMap, appliance, recipe);
+    ingredients.forEach((ingredient) => setMap(ingredientMap, ingredient.ingredient, recipe));
+    ustensils.forEach((ustensil) => setMap(ustensilMap, ustensil, recipe));
+  });
+
+  return {
+    titleMap,
+    descriptionMap,
+    applianceMap,
+    ingredientMap,
+    ustensilMap,
+  };
+};
+
+// set maps for searches
+// init object instances
+// display DOM from header function + objects
 const onLoad = () => {
-  getDOM();
+  const maps = initMaps();
+  // console.log('onLoad ~ maps', maps);
+  // console.log(maps.ustensilMap);
 };
 
 window.addEventListener('DOMContentLoaded', onLoad);
