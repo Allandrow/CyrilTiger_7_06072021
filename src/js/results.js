1df03c21e9386e765ca783c552d2f4ae8384e8d1
Cyril Tiger <cyriltiger@gmail.com>
2021-07-28 16:31:14 +0200

    created a result object for single result dom creation/handling

--- src/js/results.js
+++ src/js/results.js
@@ -1,4 +1,5 @@
-import { EMPTYSIZE, WEIGHTUNIT, LITERUNITS } from './config.js';
+import { EMPTYSIZE } from './config.js';
+import Result from './result.js';
 
 export default class Results {
   constructor() {
@@ -18,88 +19,6 @@ export default class Results {
     return this.createDOM();
   }
 
-  // return text for quantity depending of presence and content of unit
-  handleQuantityText(quantity, unit) {
-    if (unit) {
-      if (unit === WEIGHTUNIT) return `${quantity} g`;
-      if (LITERUNITS.some((item) => item === unit)) return `${quantity} L`;
-      return `${quantity} ${unit}`;
-    }
-    return `${quantity}`;
-  }
-
-  createIngredientListItem(recipeIngredient) {
-    const { ingredient, quantity, unit } = recipeIngredient;
-    const li = document.createElement('li');
-    const ingredientName = document.createElement('strong');
-
-    // Change textContent of ingredient depending of if it has a quantity and quantity unit
-    if (quantity) {
-      ingredientName.textContent = `${ingredient} : `;
-      const quantityElement = document.createElement('span');
-      const quantityText = this.handleQuantityText(quantity, unit);
-      quantityElement.textContent = quantityText;
-      li.append(ingredientName, quantityElement);
-    } else {
-      ingredientName.textContent = ingredient;
-      li.appendChild(ingredientName);
-    }
-    return li;
-  }
-
-  createResult(result) {
-    const { description, ingredients, name, time } = result;
-
-    // classNames
-    const containerBEM = 'result-card';
-    const contentBEM = `${containerBEM}__content`;
-    const headBEM = `${contentBEM}__head`;
-    const bodyBEM = `${contentBEM}__body`;
-
-    // DOM generation
-    const article = document.createElement('article');
-    article.className = containerBEM;
-
-    const content = document.createElement('div');
-    content.className = contentBEM;
-
-    const contentHead = document.createElement('div');
-    contentHead.className = headBEM;
-
-    const title = document.createElement('h2');
-    title.textContent = name;
-
-    const timer = document.createElement('div');
-    timer.className = 'timer';
-
-    const img = document.createElement('img');
-    img.src = 'dist/img/timer.svg';
-
-    const timerText = document.createElement('span');
-    timerText.textContent = `${time} min`;
-    timer.append(img, timerText);
-    contentHead.append(title, timer);
-
-    const contentBody = document.createElement('div');
-    contentBody.className = bodyBEM;
-
-    const list = document.createElement('ul');
-    list.className = 'ingredients';
-    ingredients.forEach((recipeIngredient) => {
-      const li = this.createIngredientListItem(recipeIngredient);
-      list.appendChild(li);
-    });
-
-    const paragraph = document.createElement('p');
-    paragraph.className = 'description';
-    paragraph.textContent = description;
-
-    contentBody.append(list, paragraph);
-    content.append(contentHead, contentBody);
-    article.appendChild(content);
-    return article;
-  }
-
   // clear list of results and fill with new results
   onChange(results) {
     const container = this.container;
@@ -115,7 +34,8 @@ export default class Results {
     }
 
     results.forEach((result) => {
-      const resultDOM = this.createResult(result);
+      const resultObj = new Result(result);
+      const resultDOM = resultObj.getDOM();
       container.appendChild(resultDOM);
     });
   }
